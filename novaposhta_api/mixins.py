9e1523472bcccc0989d7dab8a787c0f7cc4bccc5
demirug <pm.antondolia@gmail.com>
2022-08-17 13:32:35 +0300

    Fixed mixins

--- novaposhta_api/mixins.py
+++ novaposhta_api/mixins.py
@@ -5,6 +5,9 @@ from django.db import IntegrityError
 
 class NP_DirectJSONDataMixin:
     def __init__(self, api_json: dict):
+        if api_json is None:
+            return
+
         for key, value in api_json.items():
             handled = False
             if hasattr(self, f"handle_{key}"):
@@ -14,15 +17,17 @@ class NP_DirectJSONDataMixin:
                     attr(value)
 
             if not handled:
-                    setattr(self, key, value)
+                setattr(self, key, value)
 
 
 class NP_JSONDataMixin:
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        need_save = False
+        if self.get_json() == "":
+            return
 
+        need_save = False
         for key, value in json.loads(self.get_json()).items():
             handled = False
             if hasattr(self, f"handle_{key}"):
